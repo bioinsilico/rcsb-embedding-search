f8b4a4a777247fd6c27c93f7c35ad88ca941af93
joan <jsegurmor@gmail.com>
2024-10-02 14:22:13 -0700

    multi branch transformer

--- src/networks/transformer_pst.py
+++ src/networks/transformer_pst.py
@@ -1,5 +1,5 @@
-
 import torch.nn as nn
+from torch import cat
 from collections import OrderedDict
 
 from torch_geometric.utils import unbatch
@@ -7,20 +7,21 @@ from torch_geometric.utils import unbatch
 from dataset.pst.pst import load_pst_model
 from dataset.utils.tools import collate_seq_embeddings
 from networks.layers import ResBlock
+from networks.transformer_nn import TransformerEmbeddingCosine
 
 
 class TransformerPstEmbeddingCosine(nn.Module):
     dropout = 0.1
 
     def __init__(
-        self,
-        pst_model_path,
-        input_features=640,
-        dim_feedforward=1280,
-        hidden_layer=640,
-        nhead=10,
-        num_layers=6,
-        res_block_layers=0
+            self,
+            pst_model_path,
+            input_features=640,
+            dim_feedforward=1280,
+            hidden_layer=640,
+            nhead=10,
+            num_layers=6,
+            res_block_layers=0
     ):
         super().__init__()
 
@@ -92,6 +93,84 @@ class TransformerPstEmbeddingCosine(nn.Module):
 
     def get_weights(self):
         return [(name, param) for name, param in self.embedding.named_parameters()]
+
+
+class TransformerPstMultiBranchEmbeddingCosine(nn.Module):
+    dropout = 0.1
+
+    def __init__(
+            self,
+            pst_model_path,
+            input_features=640,
+            dim_feedforward=1280,
+            hidden_layer=640,
+            nhead=10,
+            num_layers=6,
+            res_block_layers=0,
+            num_path=2,
+            path_block_layers=6
+    ):
+        super().__init__()
+
+        self.pst_model = load_pst_model({
+            'model_path': pst_model_path
+        })
+        self.transformer_paths = nn.ModuleList([TransformerEmbeddingCosine(
+                input_features,
+                dim_feedforward,
+                hidden_layer,
+                nhead,
+                num_layers,
+                res_block_layers
+            ) for _ in range(0, num_path)])
+
+        res_in_dim = num_path * input_features
+        res_out_dim = num_path * hidden_layer
+        if path_block_layers == 0:
+            self.embedding = nn.Sequential(OrderedDict([
+                ('norm', nn.LayerNorm(res_in_dim)),
+                ('dropout', nn.Dropout(p=self.dropout)),
+                ('linear', nn.Linear(res_in_dim, res_out_dim)),
+                ('activation', nn.ReLU())
+            ]))
+        else:
+            res_block = OrderedDict([(
+                f'block{i}',
+                ResBlock(res_in_dim, res_out_dim, self.dropout)
+            ) for i in range(path_block_layers)])
+            res_block.update([
+                ('dropout', nn.Dropout(p=self.dropout)),
+                ('linear', nn.Linear(res_in_dim, res_out_dim)),
+                ('activation', nn.ReLU())
+            ])
+            self.embedding = nn.Sequential(res_block)
+
+    def graph_transformer_forward(self, graph):
+        self.pst_model.eval()
+        embedding = self.pst_model(
+            graph,
+            return_repr=True,
+            aggr=None
+        )
+        return collate_seq_embeddings(unbatch(
+            embedding[graph.idx_mask],
+            graph.batch[graph.idx_mask]
+        ))
+
+    def embedding_pooling(self, x_batch, x_mask):
+        return self.embedding(cat(
+            [transformer.embedding_pooling(x_batch, x_mask) for transformer in self.transformer_paths],
+            dim=1
+        ))
+
+    def forward(self, g_i, g_j):
+        x_batch, x_mask = self.graph_transformer_forward(g_i)
+        y_batch, y_mask = self.graph_transformer_forward(g_j)
+
+        return nn.functional.cosine_similarity(
+            self.embedding_pooling(x_batch, x_mask),
+            self.embedding_pooling(y_batch, y_mask)
+        )
 
 
 class TransformerPstCosine(nn.Module):
