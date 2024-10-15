314550b3602692b2c5b04f4a97971562f60c09e2
joan <jsegurmor@gmail.com>
2024-10-15 14:21:36 -0700

    TransformerPstMetaEmbeddingCosine

--- src/networks/transformer_pst.py
+++ src/networks/transformer_pst.py
@@ -179,6 +179,97 @@ class TransformerPstMultiBranchEmbeddingCosine(nn.Module):
         )
 
 
+class TransformerPstMetaEmbeddingCosine(nn.Module):
+    dropout = 0.1
+
+    def __init__(
+            self,
+            pst_model_path,
+            input_features=640,
+            dim_feedforward=1280,
+            hidden_layer=640,
+            nhead=10,
+            num_layers=6,
+            num_path=2,
+            path_block_layers=6
+    ):
+        super().__init__()
+
+        self.pst_model = load_pst_model({
+            'model_path': pst_model_path
+        })
+        self.transformer_paths = nn.ModuleList([nn.TransformerEncoder(nn.TransformerEncoderLayer(
+            d_model=input_features,
+            nhead=nhead,
+            dim_feedforward=dim_feedforward,
+            dropout=self.dropout,
+            batch_first=True
+        ), num_layers=num_layers) for _ in range(0, num_path)])
+
+        res_in_dim = num_path * input_features
+        res_out_dim = num_path * hidden_layer
+
+        self.transformer_meta_embedding = nn.TransformerEncoder(nn.TransformerEncoderLayer(
+            d_model=res_in_dim,
+            nhead=nhead,
+            dim_feedforward=res_out_dim,
+            dropout=self.dropout,
+            batch_first=True
+        ), num_layers=num_layers)
+
+        if path_block_layers == 0:
+            self.embedding = nn.Sequential(OrderedDict([
+                ('norm', nn.LayerNorm(res_in_dim)),
+                ('dropout', nn.Dropout(p=self.dropout)),
+                ('linear', nn.Linear(res_in_dim, res_out_dim)),
+                ('activation', nn.ReLU())
+            ]))
+        else:
+            res_block = OrderedDict([(
+                f'block{i}',
+                ResBlock(res_in_dim, res_out_dim, self.dropout)
+            ) for i in range(path_block_layers)])
+            res_block.update([
+                ('dropout', nn.Dropout(p=self.dropout)),
+                ('linear', nn.Linear(res_in_dim, res_out_dim)),
+                ('activation', nn.ReLU())
+            ])
+            self.embedding = nn.Sequential(res_block)
+
+    def graph_transformer_forward(self, graph):
+        self.pst_model.eval()
+        embedding = self.pst_model(
+            graph,
+            return_repr=True,
+            aggr=None
+        )
+        return collate_seq_embeddings(unbatch(
+            embedding[graph.idx_mask],
+            graph.batch[graph.idx_mask]
+        ))
+
+    def embedding_pooling(self, x_batch, x_mask):
+        return self.embedding(self.transformer_meta_embedding(cat(
+            [transformer(x_batch, src_key_padding_mask=x_mask) for transformer in self.transformer_paths],
+            dim=2
+        ), src_key_padding_mask=x_mask).sum(dim=1))
+
+    def validation_forward(self, x, x_mask, y, y_mask):
+        return nn.functional.cosine_similarity(
+            self.embedding_pooling(x, x_mask),
+            self.embedding_pooling(y, y_mask)
+        )
+
+    def forward(self, g_i, g_j):
+        x_batch, x_mask = self.graph_transformer_forward(g_i)
+        y_batch, y_mask = self.graph_transformer_forward(g_j)
+
+        return nn.functional.cosine_similarity(
+            self.embedding_pooling(x_batch, x_mask),
+            self.embedding_pooling(y_batch, y_mask)
+        )
+
+
 class TransformerPstCosine(nn.Module):
     dropout = 0.1
 
