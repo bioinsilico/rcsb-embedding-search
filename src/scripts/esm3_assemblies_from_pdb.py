64817611469a9a85f57b48bee5a01ddae169d0e1
joan <jsegurmor@gmail.com>
2025-08-11 15:10:55 -0700

    assemblies from pdb or cif

--- src/scripts/esm3_assemblies_from_pdb.py
+++ src/scripts/esm3_assemblies_from_pdb.py
@@ -4,7 +4,8 @@ import os.path
 
 import torch
 from biotite.structure import get_chains, chain_iter, filter_amino_acids, get_residues
-from biotite.structure.io.pdb import PDBFile
+from biotite.structure.io.pdb import PDBFile, get_structure as get_pdb_structure
+from biotite.structure.io.pdbx import CIFFile, get_structure as get_cif_structure
 from esm.models.esm3 import ESM3
 from esm.sdk.api import ESM3InferenceClient, ESMProtein, SamplingConfig
 from esm.utils.constants.models import ESM3_OPEN_SMALL
@@ -12,7 +13,7 @@ from esm.utils.structure.protein_chain import ProteinChain
 from torch.utils.data import DataLoader
 
 from dataset.esm3_embeddings_from_coord_dataset import EsmEmbeddingFromPdbDataset
-
+from scripts.esm3_embeddings_from_cif import rename_atom_attr
 
 logger = logging.getLogger(__name__)
 
@@ -37,21 +38,31 @@ if __name__ == '__main__':
 
     for s in dataloader:
         pdb_file = s[1][0]
-        pdb_id = s[0][0]
+        pdb_id, ext = os.path.splitext(pdb_file)
+        pdb_id = os.path.basename(pdb_id)
         if os.path.isfile(f"{args.out_path}/{pdb_id}.pt"):
             print(f"{pdb_file} is ready")
             continue
         print(f"Processing {pdb_file}")
-        pdb = PDBFile.read(pdb_file)
+        pdb = PDBFile.read(pdb_file) if ext == ".pdb" else CIFFile.read(pdb_file)
+        assembly_structure = get_pdb_structure(pdb) if ext == ".pdb" else get_cif_structure(
+            pdb,
+            model=1,
+            use_author_fields=False
+        )
+
         assembly_ch = []
-        for atom_ch in chain_iter(pdb.get_structure()):
+        for atom_ch in chain_iter(assembly_structure):
             ch = get_chains(atom_ch)[0]
-            atom_res = atom_ch[0][filter_amino_acids(atom_ch)]
+            if ext == ".pdb":
+                atom_ch = atom_ch[0]
+            atom_res = atom_ch[filter_amino_acids(atom_ch)]
             if len(atom_res) == 0 or len(get_residues(atom_res)[0]) < 10:
                 logger.info(f"Ignoring chain {pdb_id}.{ch} with {0 if len(atom_res) == 0 else len(get_residues(atom_res)[0])} res")
                 continue
             if len(get_chains(atom_ch)) > 1:
                 raise ValueError("Inconsistent chain ids")
+            atom_res = rename_atom_attr(atom_res)
             protein_chain = ProteinChain.from_atomarray(atom_res)
             protein = ESMProtein.from_protein_chain(protein_chain)
             protein_tensor = model.encode(protein)
