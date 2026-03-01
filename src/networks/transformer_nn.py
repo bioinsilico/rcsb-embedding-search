06c40ff25dcf0262714c658886e23924a396eeb0
joan <jsegurmor@gmail.com>
2026-03-01 10:09:45 -0800

    decoder aggregation

--- src/networks/transformer_nn.py
+++ src/networks/transformer_nn.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from collections import OrderedDict
 
@@ -67,6 +68,91 @@ class TransformerEmbeddingCosine(nn.Module):
 
     def embedding_pooling(self, x, x_mask):
         return self.embedding(self.transformer(x, src_key_padding_mask=x_mask).sum(dim=1))
+
+    def forward(self, x, x_mask, y, y_mask):
+        return nn.functional.cosine_similarity(
+            self.embedding_pooling(x, x_mask),
+            self.embedding_pooling(y, y_mask)
+        )
+
+    def get_weights(self):
+        return [(name, param) for name, param in self.embedding.named_parameters()]
+
+
+class TransformerDecoderEmbeddingCosine(nn.Module):
+    dropout = 0.1
+
+    def __init__(
+        self,
+        input_features=640,
+        dim_feedforward=1280,
+        hidden_layer=640,
+        nhead=10,
+        num_encoder_layers=6,
+        num_decoder_layers=2,
+        res_block_layers=0
+    ):
+        super().__init__()
+        # Encoder to process input sequences
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=input_features,
+            nhead=nhead,
+            dim_feedforward=dim_feedforward,
+            dropout=self.dropout,
+            batch_first=True
+        )
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_encoder_layers)
+
+        # Decoder for aggregation
+        decoder_layer = nn.TransformerDecoderLayer(
+            d_model=input_features,
+            nhead=nhead,
+            dim_feedforward=dim_feedforward,
+            dropout=self.dropout,
+            batch_first=True
+        )
+        self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=num_decoder_layers)
+
+        # Learnable query token for aggregation
+        self.query_token = nn.Parameter(torch.randn(1, 1, input_features))
+
+        # Embedding layers
+        if res_block_layers == 0:
+            self.embedding = nn.Sequential(OrderedDict([
+                ('norm', nn.LayerNorm(input_features)),
+                ('dropout', nn.Dropout(p=self.dropout)),
+                ('linear', nn.Linear(input_features, hidden_layer)),
+                ('activation', nn.ReLU())
+            ]))
+        else:
+            res_block = OrderedDict([(
+                f'block{i}',
+                ResBlock(input_features, hidden_layer, self.dropout)
+            ) for i in range(res_block_layers)])
+            res_block.update([
+                ('dropout', nn.Dropout(p=self.dropout)),
+                ('linear', nn.Linear(input_features, hidden_layer)),
+                ('activation', nn.ReLU())
+            ])
+            self.embedding = nn.Sequential(res_block)
+
+    def embedding_pooling(self, x, x_mask):
+        # Encode the input sequence
+        encoded = self.transformer_encoder(x, src_key_padding_mask=x_mask)
+
+        # Expand query token to batch size
+        batch_size = x.size(0)
+        query = self.query_token.expand(batch_size, -1, -1)
+
+        # Use decoder to aggregate encoded sequence with query token
+        aggregated = self.transformer_decoder(
+            query,
+            encoded,
+            memory_key_padding_mask=x_mask
+        )
+
+        # Take the single output token and pass through embedding
+        return self.embedding(aggregated.squeeze(1))
 
     def forward(self, x, x_mask, y, y_mask):
         return nn.functional.cosine_similarity(
